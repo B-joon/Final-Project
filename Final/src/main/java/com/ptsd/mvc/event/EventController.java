e7577e7fb6c21d4614717777a6993b8178e238f2
chacha <garam_angel@naver.com>
2021-06-20 01:58:28 +0900

    [add] event 페이지 생성

---
+++ Final/src/main/java/com/ptsd/mvc/event/EventController.java
@@ -0,0 +1,8 @@
+package com.ptsd.mvc.event;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EventController {
+
+}
---
+++ Final/src/main/java/com/ptsd/mvc/event/EventDto.java
@@ -0,0 +1,5 @@
+package com.ptsd.mvc.event;
+
+public class EventDto {
+
+}
