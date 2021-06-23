038d19a996cdf6bacedf70ed9c4a6b1d7196049a
chacha <garam_angel@naver.com>
2021-06-23 16:32:00 +0900

    [mod] 페이지 경로 수정

--- Final/src/main/java/com/ptsd/mvc/notice/EventController.java
+++ Final/src/main/java/com/ptsd/mvc/notice/EventController.java
@@ -7,11 +7,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EventController {
 	@Autowired
 	private NoticeBiz biz;
+	
+	
 	
 	@RequestMapping("/eventlist.do")
 	public String selectList(Model model, PagingCriteria cri) {
