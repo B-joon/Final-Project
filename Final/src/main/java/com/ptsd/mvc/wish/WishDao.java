4dc0b32f141741551141cdff77b63e2fc14cbe11
normal114 <normal114@naver.com>
2021-06-30 11:33:34 +0900

    [add] 찜하기 구현 중
    [add] 찜하기 구현 중

---
+++ Final/src/main/java/com/ptsd/mvc/wish/WishDao.java
@@ -0,0 +1,23 @@
+/*package com.ptsd.mvc.wish;
+
+import java.util.List;
+
+public interface WishDao {
+
+	String NAMESPACE = "board.";
+	
+	// 찜하기 추가
+	public int insertWish(WishDto dto);
+	
+	// 찜하기 삭제
+	public int deleteWish(int wish_user_no);
+	
+	// 찜하기 목록
+	public List<BoardDto> listWish(int wish_user_no);
+	
+	// 찜하기 체크
+	public int scrapChk(WishDto dto);
+	
+	
+}
+*/
---
+++ Final/src/main/java/com/ptsd/mvc/wish/WishDaoImpl.java
@@ -0,0 +1,73 @@
+/*package com.ptsd.mvc.wish;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class WishDaoImpl implements WishDao {
+
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+
+	@Override
+	public int insertWish(WishDto dto) {
+		
+		int res = 0;
+		
+		try {
+			res = sqlSession.insert(NAMESPACE + "wishinsert", dto);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return res;
+	}
+	
+	@Override
+	public int deleteWish(int wish_user_no) {
+		
+		int res = 0;
+		
+		try {
+			res = sqlSession.delete(NAMESPACE + "wishdelete", wish_user_no);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return res;
+	}
+
+	@Override
+	public <BoardDto> void listWish(int wish_user_no) {
+		
+		List<BoardDto> list = new ArrayList<BoardDto>();
+		
+		try {
+			list = sqlSession.selectList(NAMESPACE + "wishlist", wish_user_no);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return list;
+	}
+
+	@Override
+	public int scrapChk(WishDto dto) {
+		
+		int res = 0;
+		
+		try {
+			res = sqlSession.selectOne(NAMESPACE + "wishchk", dto);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return res;
+	}
+
+}
+*/
