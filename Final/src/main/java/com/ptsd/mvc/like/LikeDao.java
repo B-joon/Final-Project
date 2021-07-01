4a67327032a3f679b8482ec6399db6ed0acdd0f8
최봉준 <>
2021-07-01 15:33:24 +0900

    [add, mod] 좋아요관련 코드 추가 및 수정

--- Final/src/main/java/com/ptsd/mvc/like/LikeDao.java
+++ Final/src/main/java/com/ptsd/mvc/like/LikeDao.java
@@ -2,4 +2,9 @@ package com.ptsd.mvc.like;
 
 public interface LikeDao {
 
+	public int likeCount(LikeDto dto);
+	public int likeInsert(LikeDto dto);
+	public int likeUpdate(int productseq);
+	public int likeDel(LikeDto dto);
+	
 }
